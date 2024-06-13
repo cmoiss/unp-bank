754e3097ae3cc6e07c085e5ff5be83286ee71e4d
Caio Moisés Souza Barbalho <160979584+cmoiss@users.noreply.github.com>
2024-06-13 10:42:12 -0300

    criado - enum TipoPessoa

--- script/src/Control/TipoPessoa.java
+++ script/src/Control/TipoPessoa.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Control;
 
-/**
- *
- * @author 12824125814
- */
-public class TipoPessoa {
-    
+public enum TipoPessoa {
+    GERENTE,
+    CLIENTE
 }
