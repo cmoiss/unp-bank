4caeec868aa6a192311a9dec6a39ded0475416dd
Caio <ocaiomoises@gmail.com>
2024-06-13 23:44:47 -0300

    teste - validação para login
    verifica se o login/senha dados na tela de login são as mesmas do resultado do BD

--- script/src/Control/DAO/LoginDAO.java
+++ script/src/Control/DAO/LoginDAO.java
@@ -7,36 +7,35 @@ import java.sql.SQLException;
 import java.sql.ResultSet;
 
 public class LoginDAO {
-    
-   /* String auxiliar = null;
-    TipoPessoa tipoPessoa = null;*/
-                                 
-  
-        /*this.tipoPessoa = tipoPessoa;
-
-        if (tipoPessoa.equals(tipoPessoa.CLIENTE)) {
-            auxiliar = "tbl_cliente";
-        } else if (tipoPessoa.equals(tipoPessoa.GERENTE)) {
-            auxiliar = "tbl_gerente";
-        }*/
-    
 
     private Connection conexao;
+
+    private String solicitarLoginBD() {
+
+    }
 
     public boolean LoginCheck(String login, String senha) {
         conexao = (Connection) new dbConnect().getConnection();
         boolean Check = false;
         PreparedStatement comandoSQL;
-        ResultSet result ;
+        ResultSet result;
 
         try {
             comandoSQL = conexao.prepareStatement("SELECT*FROM tbl_Gerente WHERE login= '?' and senha= '?' ");
             comandoSQL.setString(1, login);
             comandoSQL.setString(2, senha);
             result = comandoSQL.executeQuery();
+
+            // Navegação do resultado
             if (result.next()) {
-                Check = true;
+                String loginBD = result.getString("login");
+                String senhaBD = result.getString("senha");
+
+                if (login.equals(loginBD) && senha.equals(senhaBD)) {
+                    Check = true;
+                }
             }
+
         } catch (SQLException execao) {
             throw new RuntimeException(execao);
         }
