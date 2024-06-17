4b3956bef4628275f19ccab59e7f8585908c8a5c
Lucasgfln <163592473+Lucasgfln@users.noreply.github.com>
2024-06-17 07:45:53 -0300

    carai

--- script/src/Control/DAO/dbConnect.java
+++ script/src/Control/DAO/dbConnect.java
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/working-on-code
 package Control.DAO;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-<<<<<<< HEAD
 public class dbConnect {
 
     public Connection getConnection() {
@@ -19,17 +14,5 @@ public class dbConnect {
             throw new RuntimeException(excecao);
         }
         
-=======
-
-
-public class dbConnect {
-    public Connection getConnection() {
-		try {          //"jdbc:mysql://localhost:3306/db_ubank: COLOQUE O DB CORRETO","SEU USER","SUA SENHA"
-                    return DriverManager.getConnection("jdbc:mysql://localhost:3306/db_ubank","root","admin");
-		 } 
-		 catch(SQLException excecao) {
-			throw new RuntimeException(excecao);
-		 }
->>>>>>> origin/working-on-code
     }
 }
